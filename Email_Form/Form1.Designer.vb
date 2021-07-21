f5285e1e800e9b3f916e3042e3f3619f5d483bb8
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-07-22 00:44:59 +0300

    Add files via upload

---
+++ Email_Form/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtfrom = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.lnkAttachment = New System.Windows.Forms.LinkLabel()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.lblAttachment = New System.Windows.Forms.Label()
+        Me.txtmessage = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtto = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtsubject = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(9, Byte), Integer), CType(CType(132, Byte), Integer), CType(CType(227, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(668, 41)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(502, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(163, 32)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "[ CLOSE ]"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 79)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Sender"
+        '
+        'txtfrom
+        '
+        Me.txtfrom.Location = New System.Drawing.Point(128, 79)
+        Me.txtfrom.Name = "txtfrom"
+        Me.txtfrom.Size = New System.Drawing.Size(486, 27)
+        Me.txtfrom.TabIndex = 2
+        '
+        'btnSend
+        '
+        Me.btnSend.BackColor = System.Drawing.Color.FromArgb(CType(CType(9, Byte), Integer), CType(CType(132, Byte), Integer), CType(CType(227, Byte), Integer))
+        Me.btnSend.FlatAppearance.BorderSize = 0
+        Me.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSend.ForeColor = System.Drawing.Color.White
+        Me.btnSend.Location = New System.Drawing.Point(502, 531)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(126, 34)
+        Me.btnSend.TabIndex = 12
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = False
+        '
+        'lnkAttachment
+        '
+        Me.lnkAttachment.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.lnkAttachment.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lnkAttachment.ForeColor = System.Drawing.Color.Gray
+        Me.lnkAttachment.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline
+        Me.lnkAttachment.LinkColor = System.Drawing.Color.FromArgb(CType(CType(133, Byte), Integer), CType(CType(133, Byte), Integer), CType(CType(133, Byte), Integer))
+        Me.lnkAttachment.Location = New System.Drawing.Point(33, 531)
+        Me.lnkAttachment.Name = "lnkAttachment"
+        Me.lnkAttachment.Size = New System.Drawing.Size(165, 27)
+        Me.lnkAttachment.TabIndex = 15
+        Me.lnkAttachment.TabStop = True
+        Me.lnkAttachment.Text = "Add Attachment"
+        Me.lnkAttachment.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lnkAttachment.VisitedLinkColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'lblAttachment
+        '
+        Me.lblAttachment.AutoSize = True
+        Me.lblAttachment.Location = New System.Drawing.Point(67, 484)
+        Me.lblAttachment.Name = "lblAttachment"
+        Me.lblAttachment.Size = New System.Drawing.Size(123, 20)
+        Me.lblAttachment.TabIndex = 22
+        Me.lblAttachment.Text = "ATTACHMENT"
+        '
+        'txtmessage
+        '
+        Me.txtmessage.Location = New System.Drawing.Point(128, 269)
+        Me.txtmessage.Multiline = True
+        Me.txtmessage.Name = "txtmessage"
+        Me.txtmessage.Size = New System.Drawing.Size(486, 189)
+        Me.txtmessage.TabIndex = 21
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(75, 272)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 20)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Body"
+        '
+        'txtto
+        '
+        Me.txtto.Location = New System.Drawing.Point(128, 143)
+        Me.txtto.Name = "txtto"
+        Me.txtto.Size = New System.Drawing.Size(486, 27)
+        Me.txtto.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(94, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 20)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "To"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(57, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 20)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Subject"
+        '
+        'txtsubject
+        '
+        Me.txtsubject.Location = New System.Drawing.Point(128, 209)
+        Me.txtsubject.Name = "txtsubject"
+        Me.txtsubject.Size = New System.Drawing.Size(486, 27)
+        Me.txtsubject.TabIndex = 18
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(668, 591)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtfrom)
+        Me.Controls.Add(Me.lblAttachment)
+        Me.Controls.Add(Me.txtmessage)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtto)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtsubject)
+        Me.Controls.Add(Me.lnkAttachment)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtfrom As TextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents lnkAttachment As LinkLabel
+    Friend WithEvents Label7 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents lblAttachment As Label
+    Friend WithEvents txtmessage As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtto As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtsubject As TextBox
+End Class
---
+++ Email_Form/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Net.Mail
+
+Public Class Form1
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub lnkAttachment_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkAttachment.LinkClicked
+        With OpenFileDialog1
+            .FileName = ""
+            .Multiselect = False
+        End With
+        If OpenFileDialog1.ShowDialog = 1 Then
+            lblAttachment.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+    Dim accountFrom As String = "youremail@gmail.com"
+    Dim message As New MailMessage
+    Dim smtp As New SmtpClient
+    Dim instance As IDisposable
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+        Try
+            Dim Mail As New MailMessage
+            Mail.To.Add(txtto.Text)
+            Mail.Subject = txtsubject.Text
+            Mail.From = New MailAddress(accountFrom)
+            Mail.Body = txtmessage.Text
+            Dim attach As New Attachment(OpenFileDialog1.FileName)
+            Mail.Attachments.Add(attach)
+            Dim SMTP As New SmtpClient("smtp.gmail.com")
+            SMTP.Port = 587
+            SMTP.Credentials = New System.Net.NetworkCredential(accountFrom, "yourpasscode")
+            SMTP.EnableSsl = True
+            SMTP.Send(Mail)
+            MsgBox("Sent Successfully")
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtfrom.Text = accountFrom
+        txtfrom.Enabled = False
+
+    End Sub
+End Class
